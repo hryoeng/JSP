5fa2db0171c1094483a2800b30fce764183da516
hryeong <gpfud9109@gmail.com>
2024-02-13 17:21:15 +0900

    11장 jstl

---
+++ src/main/java/com/util/CookieManager.java
@@ -0,0 +1,58 @@
+package com.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieManager {
+
+    //1. 쿠키 생성
+    // 이름, 값, 유지기간 파라메터 새로운 쿠키 생성
+    /*
+    * 문서화 주석 대상의 요약 설명이다.
+    * @param : cName 쿠키 이름
+    * @param : xValue 쿠키 값
+    * @param : cTime 쿠키 유지기간
+    *   @return : void
+    */
+
+    /**
+     *
+     * 쿠키 생성 메소드
+     * @Param : cName : 쿠키이름
+     * @Return : void
+     * @throws : 예외처리 하는 방식
+     *
+     */
+
+    public static void makeCookie(HttpServletResponse response, String cName, String cValue, int cTime){
+
+        //쿠키생성
+        /*컨트롤 쉬프트 에프 로 찾아서 써도 된다...*/
+        Cookie cookie = new Cookie(cName,cValue);
+        cookie.setPath("/");    // 경로설정
+        cookie.setMaxAge(cTime);    // 유지기간 설정
+        response.addCookie(cookie); // 응답 객체에 추가
+    }
+    
+    //2. 쿠키 읽기
+    public static String readCookie(HttpServletRequest request, String cName){
+        String cookieValue = "";    //반환 값
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie c : cookies){
+                String cookieName = c.getName();
+                if(cookieName.equals(cName)){
+                    cookieValue = c.getValue(); // 반환 값을 담아줌
+                }
+            }
+        }
+        
+        return cookieValue;
+    }
+    
+    //3. 쿠키 삭제
+    public static void deleteCookie(HttpServletResponse response, String cName){
+        makeCookie(response, cName, "", 0);
+    }
+}
